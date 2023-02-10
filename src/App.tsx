059ac25da55d8aac8fbd2e3f80240f7869d4804b
Jerwinn <Jerwin@live.co.uk>
2023-02-10 02:49:32 +0000

    Working on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-
+import Navbar from "@/scenes/navbar";
 
 function App() {
   
 
   return (
-    <div className="App">
-    
+    <div className="App bg-gray-20">
+     <Navbar/>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -13,18 +13,28 @@ module.exports = {
         "primary-500": "#FF6B66",
         "secondary-400": "#FFCD5B",
         "secondary-500": "#FFC132",
-        },
-        backgroundImage: (theme) => ({
-          "gradient-yellowred":
-            "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-          "mobile-home": "url('./assets/HomePageGraphic.png')",
-        }),
-        fontFamily: {
-          dmsans: ["DM Sans", "sans-serif"],
-          montserrat: ["Montserrat", "sans-serif"],
-        },
-        
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [],
-}
+};
