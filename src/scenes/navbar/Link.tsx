64797c6c5c1002362a8fe40d705efcf1d42e3b61
Jerwinn <Jerwin@live.co.uk>
2023-02-12 01:17:18 +0000

    still unfinished navbar1

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -2,11 +2,15 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
     page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
 
 }
 
 const Link = ({
     page,
+    selectedPage,
+    setSelectedPage,
 
 }: Props) => {
     const lowerCasePage = page.toLowerCase().replace(/ /g, "")
@@ -17,7 +21,7 @@ const Link = ({
             transition duration-500 hover:text-primary-300
         `}
         href={`#${lowerCasePage}`}
-        onClick={}>
+        onClick={() => setSelectedPage(lowerCasePage)}>
         {page}
     </AnchorLink>
   )
